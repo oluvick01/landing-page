27d4f47d9bd25ea9f57d8b273c9e15c3c9c04fde
victor oluwolue <victoroluwole01@gmail.com>
2025-05-02 21:32:25 -1200

    making correcton on last commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-button');
 const menuItem = document.querySelector('.menu-item');
 const menuNav = document.querySelector('.menu-nav')
-
+const heroTxt = document.querySelector('.hero-txt')
 
 
 
@@ -9,9 +9,12 @@ menuBtn.addEventListener('click', (e) => {
     if (menuItem.classList.contains('hidden')){
         menuItem.classList.remove('hidden')
         menuItem.style.display = 'block';
+        heroTxt.style.display = 'none';
+        
     }else{
         menuItem.classList.add('hidden');
         menuItem.style.display = 'none';
+        heroTxt.style.display = 'block';
     }
 
 })
---
+++ readme.md
@@ -0,0 +1,1 @@
+## working on responsiveness of a landing page
